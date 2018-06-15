9222b019000d7a376bfc715cdd4fb33cfc5651dc
stavros11 <stavros11@users.noreply.github.com>
2018-06-15 17:42:50 -0400

    Added options

--- train_multiple_exponents.py
+++ train_multiple_exponents.py
@@ -5,19 +5,28 @@ Created on Fri Jun 15 12:01:08 2018
 @author: Stavros
 """
 
+import numpy as np
 from data.loaders import TrainingData
 from data.directories import models_critical_save_dir, metrics_critical_save_dir
-from networks.train import TrainerCritical
+from data.directories import multiple_exponents_dir
+from networks.train import TrainerCritical, create_directory
+from networks.consecutive import upsampling
 from argparse import ArgumentParser
+from scipy.stats import linregress
 
 parser = ArgumentParser()
+parser.add_argument('-C', type=int, default=1, help='number of calculations')
+parser.add_argument('-UP', type=int, default=3, help='number of upsamplings')
+parser.add_argument('-PRreg', type=bool, default=True, help='print regression')
+parser.add_argument('-TPF', type=bool, default=False, help='calculate two point function')
+parser.add_argument('-CORR', type=bool, default=False, help='calculate correlation length')
+
 parser.add_argument('-RGWD', type=bool, default=False, help='well defined RG')
 parser.add_argument('-L', type=int, default=16, help='output size')
 parser.add_argument('-nTR', type=int, default=40000, help='train samples')
 parser.add_argument('-nTE', type=int, default=100000, help='test samples')
 parser.add_argument('-TRS', type=int, default=40000, help='train size')
 parser.add_argument('-VALS', type=int, default=5000, help='validation size')
-parser.add_argument('-C', type=int, default=1, help='number of calculations')
 parser.add_argument('-VER', type=int, default=1, help='version for name')
 
 parser.add_argument('-PBC', type=bool, default=True, help='use PBC padding')
@@ -43,8 +52,19 @@ args.metrics_dir= metrics_critical_save_dir
 
 data = TrainingData(args)
 trainer = TrainerCritical(args)
-
+observables = []
 for iC in range(args.C):
     trainer.train(data, run_time=iC)
+    observables.append(upsampling(data.test_out, trainer.model, args))
     
+    create_directory(multiple_exponents_dir+'/%s'%trainer.name)
     
+    if args.PRreg:
+        print('Beta:')
+        print(linregress(np.log10(observables[0]), np.log10(observables[1])))
+        print('Gamma:')
+        print(linregress(np.log10(observables[0]), np.log10(observables[3])))
+
+np.save('%s/%s/observables%d.npy'%(multiple_exponents_dir, 
+                                      trainer.name,
+                                      iC+1), observables)    
