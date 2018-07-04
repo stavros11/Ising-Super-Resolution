5028294679097d57a6cf71553fdcfecc9fe74319
stavros11 <stavros11@users.noreply.github.com>
2018-07-04 13:36:49 +0300

    Added create directory function again

--- networks/utils.py
+++ networks/utils.py
@@ -5,6 +5,7 @@ Created on Sat Jun 16 12:00:24 2018
 @author: Stavros
 """
 
+from os import path, mkdir
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 
@@ -12,3 +13,8 @@ def set_GPU_memory(fraction=0.3):
     config = tf.ConfigProto()
     config.gpu_options.per_process_gpu_memory_fraction = fraction
     set_session(tf.Session(config=config))
+    
+def create_directory(d):
+    ## Create directory if it doesn't exist ##
+    if not path.exists(d):
+        mkdir(d)
