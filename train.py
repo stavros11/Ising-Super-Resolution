15b9d42806dbbd5df40c162742e1bca2c2718ba1
stavros11 <stavros11@users.noreply.github.com>
2018-07-04 13:36:26 +0300

    Fixed list arguments in parser

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ Created on Thu Jun 14 16:18:23 2018
 @author: Stavros
 """
 
+from networks.utils import set_GPU_memory
 from data.loaders import TrainingData
 from argparse import ArgumentParser
 
@@ -12,8 +13,9 @@ parser = ArgumentParser()
 parser.add_argument('-CR', type=bool, default=False, help='critical data')
 parser.add_argument('-RGWD', type=bool, default=False, help='well defined RG')
 parser.add_argument('-L', type=int, default=16, help='output size')
-parser.add_argument('-Tind', type=list, default=[], help='temperatures indices to train')
+parser.add_argument('-Tind', nargs='+', type=int, default=None, help='temperatures indices to train')
 
+parser.add_argument('-GPU', type=float, default=0.4, help='GPU memory fraction')
 parser.add_argument('-nTR', type=int, default=40000, help='train samples')
 parser.add_argument('-nTE', type=int, default=100000, help='test samples')
 parser.add_argument('-TRS', type=int, default=40000, help='train size')
@@ -22,8 +24,8 @@ parser.add_argument('-VER', type=int, default=1, help='version for name')
 
 parser.add_argument('-PBC', type=bool, default=True, help='use PBC padding')
 parser.add_argument('-ACT', type=str, default='relu', help='hidden activation')
-parser.add_argument('-HF', type=list, default=[64,32], help='hidden filters list')
-parser.add_argument('-K', type=list, default=[5,1,3], help='kernel size list')
+parser.add_argument('-HF', nargs='+', type=int, default=None, help='hidden filters list')
+parser.add_argument('-K', nargs='+', type=int, default=None, help='kernels list')
 
 parser.add_argument('-OPT', type=str, default='Adam', help='optimizer')
 parser.add_argument('-CE', type=bool, default=True, help='use cross-entropy')
@@ -37,33 +39,46 @@ parser.add_argument('-ESpat', type=int, default=50, help='early stopping patienc
 parser.add_argument('-ESdelta', type=float, default=0.0001, help='early stopping delta')
 parser.add_argument('-VB', type=int, default=0, help='verbose')
 
-
-args = parser.parse_args()
-
-### Check data sizes ###
-if args.TRS > args.nTR:
-    args.TRS = args.nTR
-if args.VALS > args.nTE:
-    args.VALS = args.nTE
-
-if args.CR:
-    from data.directories import models_critical_save_dir, metrics_critical_save_dir
-    from networks.train import TrainerCritical
-    args.model_dir = models_critical_save_dir
-    args.metrics_dir= metrics_critical_save_dir
+def main(args):
+    set_GPU_memory(fraction=args.GPU)
     
-    trainer = TrainerCritical(args)
+    ### HF, K default values ###
+    if args.HF == None:
+        args.HF = [64, 32]
+    if args.K == None:
+        args.K = [5, 1, 3]
     
-else:
-    from data.directories import models_save_dir, metrics_save_dir, T_list
-    from networks.train import TrainerTemp
-    args.model_dir = models_save_dir
-    args.metrics_dir = metrics_save_dir
+    ### Check data sizes ###
+    if args.TRS > args.nTR:
+        args.TRS = args.nTR
+    if args.VALS > args.nTE:
+        args.VALS = args.nTE
     
-    if args.Tind == []:
-        args.Tind = range(len(T_list))
-    args.T_list = T_list[args.Tind]
+    print('Hidden Filters: ' + str(args.HF))
+    print('Kernels: ' + str(args.K))
     
-    trainer = TrainerTemp(args)
-
-trainer.train(TrainingData(args))
+    if args.CR:
+        from data.directories import models_critical_save_dir, metrics_critical_save_dir
+        from networks.train import TrainerCritical
+        args.model_dir = models_critical_save_dir
+        args.metrics_dir= metrics_critical_save_dir
+        
+        trainer = TrainerCritical(args)
+        
+    else:
+        from data.directories import models_save_dir, metrics_save_dir, T_list
+        from networks.train import TrainerTemp
+        args.model_dir = models_save_dir
+        args.metrics_dir = metrics_save_dir
+        
+        if args.Tind == None:
+            args.Tind = range(len(T_list))
+        
+        args.T_list = T_list[args.Tind]
+        
+        trainer = TrainerTemp(args)
+    
+    trainer.train(TrainingData(args))
+    return
+    
+main(parser.parse_args())
