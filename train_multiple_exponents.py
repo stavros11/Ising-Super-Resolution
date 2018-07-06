6f1400e4dcef07cd2203bbcc06f4e38e5e70e583
stavros11 <stavros11@users.noreply.github.com>
2018-07-06 22:33:48 +0300

    Fixed lists in argparse

--- train_multiple_exponents.py
+++ train_multiple_exponents.py
@@ -19,8 +19,8 @@ parser = ArgumentParser()
 parser.add_argument('-C', type=int, default=1, help='number of calculations')
 parser.add_argument('-UP', type=int, default=3, help='number of upsamplings')
 parser.add_argument('-PRreg', type=bool, default=True, help='print regression')
-parser.add_argument('-TPF', type=bool, default=False, help='calculate two point function')
-parser.add_argument('-CORR', type=bool, default=False, help='calculate correlation length')
+parser.add_argument('-TPF', type=bool, default=True, help='calculate two point function')
+parser.add_argument('-CORR', type=bool, default=True, help='calculate correlation length')
 
 parser.add_argument('-GPU', type=float, default=0.4, help='GPU memory fraction')
 parser.add_argument('-RGWD', type=bool, default=False, help='well defined RG')
@@ -28,13 +28,13 @@ parser.add_argument('-L', type=int, default=16, help='output size')
 parser.add_argument('-nTR', type=int, default=40000, help='train samples')
 parser.add_argument('-nTE', type=int, default=100000, help='test samples')
 parser.add_argument('-TRS', type=int, default=40000, help='train size')
-parser.add_argument('-VALS', type=int, default=5000, help='validation size')
+parser.add_argument('-VALS', type=int, default=3000, help='validation size')
 parser.add_argument('-VER', type=int, default=1, help='version for name')
 
 parser.add_argument('-PBC', type=bool, default=True, help='use PBC padding')
 parser.add_argument('-ACT', type=str, default='relu', help='hidden activation')
-parser.add_argument('-HF', type=list, default=[64,32], help='hidden filters list')
-parser.add_argument('-K', type=list, default=[6,1,3], help='kernel size list')
+parser.add_argument('-HF', nargs='+', type=int, default=None, help='hidden filters list')
+parser.add_argument('-K', nargs='+', type=int, default=None, help='kernels list')
 
 parser.add_argument('-OPT', type=str, default='Adam', help='optimizer')
 parser.add_argument('-CE', type=bool, default=True, help='use cross-entropy')
@@ -48,36 +48,45 @@ parser.add_argument('-ES', type=bool, default=False, help='early stopping')
 parser.add_argument('-ESpat', type=int, default=50, help='early stopping patience')
 parser.add_argument('-ESdelta', type=float, default=0.001, help='early stopping delta')
 
-args = parser.parse_args()
 
-args.CR = True
-args.model_dir = models_critical_save_dir
-args.metrics_dir= metrics_critical_save_dir
-set_GPU_memory(fraction=args.GPUF)
-
-if args.PRreg:
-    L0 = int(np.log2(args.L))
-    L_list = 2**np.arange(L0, L0+1+args.UP)
-
-data = TrainingData(args)
-trainer = TrainerCritical(args)
-observables = []
-for iC in range(args.C):
-    trainer.train(data, run_time=iC)
-    obs = upsampling(data.test_out, trainer.model, args)
-    observables.append(obs)
+def main(args):
+    args.CR = True
+    args.model_dir = models_critical_save_dir
+    args.metrics_dir= metrics_critical_save_dir
+    set_GPU_memory(fraction=args.GPU)
+    
+    ### HF, K default values ###
+    if args.HF == None:
+        args.HF = [64, 32]
+    if args.K == None:
+        args.K = [5, 1, 3]
     
     if args.PRreg:
-        print('Beta:')
-        print(linregress(np.log10(L_list), np.log10(obs[0])))
-        print('Gamma:')
-        print(linregress(np.log10(L_list), np.log10(obs[2])))
-        if args.TPF:
-            print('Eta1:')
-            print(linregress(np.log10(L_list/2.0), np.log10(obs[7])))
-            print('Eta2:')
-            print(linregress(np.log10(L_list/4.0), np.log10(obs[8])))
+        L0 = int(np.log2(args.L))
+        L_list = 2**np.arange(L0, L0+1+args.UP)
+    
+    data = TrainingData(args)
+    trainer = TrainerCritical(args)
+    observables = []
+    for iC in range(args.C):
+        trainer.train(data, run_time=iC)
+        obs = upsampling(data.test_out, trainer.model, args)
+        observables.append(obs)
+        
+        if args.PRreg:
+            print('Beta:')
+            print(linregress(np.log10(L_list), np.log10(obs[0])))
+            print('Gamma:')
+            print(linregress(np.log10(L_list), np.log10(obs[2])))
+            if args.TPF:
+                print('Eta1:')
+                print(linregress(np.log10(L_list/2.0), np.log10(obs[7])))
+                print('Eta2:')
+                print(linregress(np.log10(L_list/4.0), np.log10(obs[8])))
+    
+    create_directory(multiple_exponents_dir)
+    np.save('%s/%s_C%dUP%d.npy'%(multiple_exponents_dir, trainer.name,
+                             args.C, args.UP), np.array(observables))
 
-create_directory(multiple_exponents_dir)
-np.save('%s/%s_C%dUP%d.npy'%(multiple_exponents_dir, trainer.name,
-                         args.C, args.UP), np.array(observables))
+
+main(parser.parse_args())
