d350829e0f9e74dc04deaf0889305f9086d47989
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-07-08 16:07:14 +0300

    Added batch calculation

--- networks/consecutive.py
+++ networks/consecutive.py
@@ -50,3 +50,42 @@ def upsampling(init_data, model, args):
         print('%d / %d upsamplings done!'%(i+1, args.UP))
         
     return np.array(obs).T
+
+def upsampling_batches(init_data, model, args):
+    Tc = 2.0 / np.log(1 + np.sqrt(2))
+    
+    get_observables = create_obs_function(args)
+    duplication_function = create_duplication_function(args)
+
+    obs_init = get_observables(init_data[:,:,:,0], Tc)
+    obs = np.zeros([args.UP + 1, len(obs_init)])
+    obs[0] = obs_init
+    
+    state = np.copy(init_data)
+    for i in range(args.NUP):
+        model = duplication_function(model, state.shape)
+        cont_state = model.predict(state)
+        
+        state = (cont_state > np.random.random(cont_state.shape)).astype(np.int)
+        
+        obs[i+1] = get_observables(state[:,:,:,0], Tc)
+        print('%d / %d upsamplings done!'%(i+1, args.UP))
+    
+    n_batches = state.shape[0] // args.CBS
+    for b in range(n_batches):
+        batch = state[b * args.CBS : (b+1) * args.CBS]
+        
+        for i in range(args.NUP, args.UP):
+            model = duplication_function(model, batch.shape)
+            cont_batch = model.predict(batch)
+            
+            batch = (cont_batch > np.random.random(cont_batch.shape)).astype(np.int)
+            
+            obs[i+1] += get_observables(batch[:,:,:,0], Tc)
+        
+        print('%d / %d batch done!'%(b+1, n_batches))
+        
+    # Normalize observables calculated with batches
+    obs[args.NUP+1:] = obs[args.NUP+1:] / n_batches
+    
+    return obs.T
