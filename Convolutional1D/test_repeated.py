eb5fc89dcbc9bcf25cd6c29a017aa5eaa897bf32
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-08-13 18:20:27 +0300

    Update test_repeated.py

--- Convolutional1D/test_repeated.py
+++ Convolutional1D/test_repeated.py
@@ -10,16 +10,20 @@ from data.directories import quantities_rep_dir, T_list
 from data.loaders import read_file, add_index, temp_partition
 from data.model_loader import ModelLoader
 from networks.utils import set_GPU_memory, create_directory, calculate_observables_rep
+from networks.architectures import make_prediction
 # Returns 7 observables 
 # [Mag, En, Susc, specHeat, Mag2, Mag4, En2]
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('-L', type=int, default=16, help='output size')
+parser.add_argument('-L', type=int, default=32, help='output size')
 parser.add_argument('-UP', type=int, default=3, help='number of upsamplings')
 parser.add_argument('-GPU', type=float, default=0.3, help='GPU memory fraction')
 
 parser.add_argument('-Mind', type=int, default=0, help='model index')
+parser.add_argument('-ACT', type=str, default='relu', help='hidden activation')
+parser.add_argument('-HF', nargs='+', type=int, default=None, help='hidden filters list')
+parser.add_argument('-K', nargs='+', type=int, default=None, help='kernels list')
 
 parser.add_argument('-nTE', type=int, default=10000, help='test samples')
 parser.add_argument('-TEST', type=int, default=10000, help='test size')
@@ -33,9 +37,10 @@ def main(args):
     if args.TEST > args.nTE:
         args.TEST = args.nTE
         
-    obs = np.zeros([args.UP, len(args.Tind), 3, 7])
+    obs = np.zeros([args.UP, len(T_list), 3, 7])
     ## WARNING: Does not contain original MC observables
     
+    pred_cont = []
     ## First upsampling
     T_ren = 2.0 / np.arccosh(np.exp(2.0 / T_list))
     for (iT, T) in enumerate(T_ren):
@@ -44,31 +49,33 @@ def main(args):
         model.update_temperature(T=T_model)
                 
         ## Make predictions ##
-        pred_cont = model.graph.predict(temp_partition(data_mc, iT, n_samples=args.nTE))
+        pred_cont.append(make_prediction(
+                data_in=add_index(temp_partition(data_mc, iT, n_samples=args.nTE)),
+                graph=model.graph, hid_filters=args.HF, kernels=args.K, hid_act=args.ACT))
+                
                 
         ## Calculate observables ##
-        obs[iT] = calculate_observables(
-                temp_partition(data.test_out[:,:,:,0], iT, n_samples=args.nTE),
-                data_in[:,:,:,0], pred_cont[:,:,:,0], T=T)
-                                       
-        print('Temperature %d / %d done!'%(iT+1, len(args.Tind)))
+        obs[0, iT] = calculate_observables_rep(pred_cont[iT][:,:,0], Tr=T)
+        print('Temperature %d / %d done!'%(iT+1, len(T_list)))
+    print('\nUpsampling 1 / %d completed!\n'%args.UP)
     
-    for iUP in range(args.UP):
-        for (iT, T) in enumerate(T_list):
-            ## Update model temperature ##
-            model.update_temperature(T=T)
-                
+    for iUP in range(1, args.UP):
+        T_ren = 2.0 / np.arccosh(np.exp(2.0 / T_ren))
+        for (iT, T) in enumerate(T_ren):
+            T_model = T_list[np.abs(T - T_list).argmin()]
+            model.update_temperature(T=T_model)
+                    
             ## Make predictions ##
-            data_in = temp_partition(data.test_in, iT, n_samples=args.nTE)
-            pred_cont = model.graph.predict(data_in)
-                
+            pred_cont[iT] = make_prediction(
+                    data_in=add_index(temp_partition(data_mc, iT, n_samples=args.nTE)),
+                    graph=model.graph, hid_filters=args.HF, kernels=args.K, hid_act=args.ACT)
+
             ## Calculate observables ##
-            obs[iT] = calculate_observables(
-                    temp_partition(data.test_out[:,:,:,0], iT, n_samples=args.nTE),
-                    data_in[:,:,:,0], pred_cont[:,:,:,0], T=T)
-                                       
-            print('Temperature %d / %d done!'%(iT+1, len(args.Tind)))
+            obs[iUP, iT] = calculate_observables_rep(pred_cont[iT][:,:,0], Tr=T)
+            print('Temperature %d / %d done!'%(iT+1, len(T_list)))
         
+        print('\nUpsampling %d / %d completed!\n'%(iUP+1, args.UP))
+            
     ## Save observables ##
     create_directory(quantities_rep_dir)
     np.save(quantities_rep_dir + '/%s.npy'%model.name, np.array(obs))
