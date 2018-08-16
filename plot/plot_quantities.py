b1b71444dea7db3bc4a6664e79e9223aaa426aa9
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-08-16 16:49:08 +0300

    Changed colours

--- plot/plot_quantities.py
+++ plot/plot_quantities.py
@@ -6,9 +6,11 @@ Created on Mon Jul  2 17:12:52 2018
 """
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
-rcParams.update({'font.size': 32})
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.rcParams.update({'font.size': 36})
 
 from plot_directories import T_list, quantities_dir
 # Use this T_list when plot_directories module is not available
@@ -23,7 +25,7 @@ from plot_directories import T_list, quantities_dir
 #        tpf(L/4), S0, S1, S2]
 
 # Load data (fix .npy directory here)
-NAME = 'Simple2D16relu_L3_64_16_16_K3333_PBC_MReg0.10EReg0.30_OLD'
+NAME = 'Simple2D16relu_L3_64_16_16_K3333_MReg0.10EReg0.30_OLD'
 obs = np.load('%s/%s.npy'%(quantities_dir, NAME))
 
 # Use rounding instead of sampling for the five lowest temperatures 
@@ -42,23 +44,33 @@ def plot_one(q=0, figsize=(8, 5), L=16):
     
     plt.show()
     
-def plot_four(figsize=(14, 8), L=16, linewidth=1.5, save=False):
+def plot_four(figsize=(14, 8), L=16, save=False):
     # plots the four plots (M, E, chi, Cv)
     plt.figure(figsize=figsize)
     ylab = ['$M$', '$E$', '$\chi $', '$C_V$']
     for q in range(4):
         plt.subplot(221 + q)
-        plt.plot(T_list, obs[:, 0, q], color='blue', label='%dx%d MC'%(L, L),
-                 linewidth=linewidth)
-        plt.plot(T_list, obs[:, 1, q], '--', color='green', 
-                 label='%dx%d RG'%(L//2, L//2), linewidth=linewidth)
+        plt.plot(T_list, obs[:, 0, q], color='blue', label=''.join([r'%d'%L, r'$\times$', r'%d'%L, r' MC']),
+                 linewidth=3.5)
+        plt.plot(T_list, obs[:, 1, q], '--', color='blue', 
+                 label=''.join([r'%d'%(L//2), r'$\times$', r'%d'%(L//2), r' RG']), linewidth=3.5, alpha=0.3)
         
-        plt.plot(T_list, obs[:, -1, q], '*', color='red', label='%dx%d SR'%(L, L))
-        plt.axvline(x = 2 / np.log(1 + np.sqrt(2)), linestyle='--', color='k')
-        if q == 0:
-            plt.legend()
+        plt.plot(T_list, obs[:, -1, q], 'o--', color='red', label=''.join([r'%d'%L, r'$\times$', r'%d'%L, r' SR']),
+                 linewidth=3.0, markersize=10)
+        
+        if q < 2:
+            plt.locator_params(axis='y', nbins=5)
+        else:
+            plt.locator_params(axis='y', nbins=6)
+        
+        plt.axvline(x = 2 / np.log(1 + np.sqrt(2)), linestyle='--', color='k', linewidth=2.5)
+        plt.xlim([0, 4.6])
+        
+        if q == 1:
+            plt.legend(loc='upper left')
         plt.xlabel('$T$')
         plt.ylabel(ylab[q])
+        
     
     if save:
         plt.savefig('%s.pdf'%NAME)
