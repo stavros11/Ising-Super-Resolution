fbb450762288e9fad79718b2deaf816aee9ffabe
stavros11 <stavros11@users.noreply.github.com>
2018-08-28 00:14:11 +0200

    Notebook dir

--- plot/plot_directories.py
+++ plot/plot_directories.py
@@ -9,6 +9,7 @@ from numpy import linspace
 
 BASIC_DIR = 'C:/Users/Stavros.SAVVAS-PROBOOK/Documents/Scripts_and_Programs/SR_results/'
 BASIC_DIR = 'C:/Users/Stavros/Documents/Scripts_and_programs/SR_results/'
+BASIC_DIR = 'C:/Users/Stavros.SAVVAS-PROBOOK/Documents/Scripts_and_Programs/SR_results/'
 
 ## Temperature list ##
 T_list = linspace(0.01, 4.538, 32)
