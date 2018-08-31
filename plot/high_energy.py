37268a6317375f824fbbcdf8f954e046fe410e8e
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-08-31 16:48:52 +0200

    Playing

---
+++ plot/high_energy.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 31 14:32:26 2018
+
+@author: Admin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import utils.data_functions as df
+from plot_directories import T_list
+from plot_directories import output_dir
+from utils.decimations import block_sum
+from os import path
+
+def read_sr(name):
+    sr = np.load(path.join(output_dir, name, 'T%.4f.npy'%T_list[iT]))
+    if len(sr.shape) > 3:
+        sr = sr[:, :, :, 0]
+        
+    return (sr > np.random.random(sr.shape)).astype(np.int)
+
+def calc_nn(states):
+    x = 2 * states - 1
+    
+    nn1, nn2 = np.zeros(x.shape), np.zeros(x.shape)
+    
+    nn1[:, :-1] = x[:, 1:] * x[:, :-1]
+    nn1[:, -1] = x[:, 0] * x[:, -1]
+    
+    nn2[:, :, :-1] = x[:, :, 1:] * x[:, :, :-1]
+    nn2[:, :, -1] = x[:, :, 0] * x[:, :, -1]
+    
+    return np.array([nn1, nn2]).transpose((1, 2, 3, 0))
+
+iT = 20
+mc = df.temp_partition(df.read_file(df.data_directory_select(3), L=16), iT)
+sr = read_sr('Simple2D16relu_L2_64_32_K333_PBC_MReg0.00EReg0.30B1000')
+sr_old = read_sr('Simple2D16relu_L3_64_16_16_K3333_MReg0.10EReg0.30B1000_OLD')
+
+nn_list = [calc_nn(x) for x in [mc, sr, sr_old]]
+
+plt.figure(figsize=(10, 4))
+plt.subplot(131)
+plt.hist(block_sum(mc).ravel(), bins=5)
+plt.ylim((0,350000))
+plt.subplot(132)
+plt.hist(block_sum(sr).ravel(), bins=5)
+plt.ylim((0,350000))
+plt.subplot(133)
+plt.hist(block_sum(sr_old).ravel(), bins=5)
+plt.ylim((0,350000))
--- plot/plot_sum_hist.py
+++ plot/plot_sum_hist.py
@@ -20,7 +20,7 @@ NAME = 'Simple2D16relu_L3_64_16_16_K3333_MReg0.10EReg0.30B1000_OLD'
 
 def read_data(iT):
     ## Read MC and SR data ##
-    mc = df.temp_partition(df.read_file(df.data_directory_select(1), L=16), iT)
+    mc = df.temp_partition(df.read_file(df.data_directory_select(3), L=16), iT)
     output = np.load('%s/%s/T%.4f.npy'%(output_dir, NAME, T_list[iT]))
     sampled = (output > np.random.random(output.shape)).astype(np.int)
     
@@ -80,73 +80,73 @@ def plot_block_sum(iT=[10,15,20], figsize=(10, 6), save=False):
     else:
         plt.show()
         
-#plot_block_sum(figsize=(25,20), save=True)
+plot_block_sum(figsize=(25,20), save=True)
         
 ## What happens with ties (for Roger) ##
-def tie_reason(states):
-    n_samples, Ly, Lx = states.shape
-    bs = block_sum(states)
-    n_ties = (bs == 2).sum()
-    keeper = (bs == 2).astype(np.int).repeat(2, axis=1).repeat(2, axis=2)
-    
-    tied_state = keeper * states
-    
-    rows = (tied_state[:, 1:] * tied_state[:, :-1])[:, np.arange(0, Ly, 2)].sum()
-    cols = (tied_state[:, :, 1:] * tied_state[:, :, :-1])[:, :, np.arange(0, Lx, 2)].sum()
-    
-    return np.array([rows, cols, n_ties - rows - cols])
-    
-mc_sums, sr_sums = np.zeros([32, 3]), np.zeros([32, 3])
-
-for iT in range(32):
-    mc = df.temp_partition(df.read_file(df.data_directory_select(1), L=16), iT)
-    output = np.load('%s/%s/T%.4f.npy'%(output_dir, NAME, T_list[iT]))
-    sampled = (output > np.random.random(output.shape)).astype(np.int)
-    
-    mc_sums[iT] = tie_reason(mc)
-    sr_sums[iT] = tie_reason(sampled)
-    
-    print(iT)
-
-
-rcParams.update({'font.size': 48})
-label_size = 52
-from mpl_toolkits.axes_grid.inset_locator import inset_axes
-
-
-cut_iT = 10
-mc_norm = 100*mc_sums[cut_iT:] / mc_sums[cut_iT:].sum(axis=1)[:, np.newaxis]
-sr_norm = 100*sr_sums[cut_iT:] / sr_sums[cut_iT:].sum(axis=1)[:, np.newaxis]
-
-plt.figure(figsize=(30, 8))
-plt.subplot(121)
-plt.plot(T_list[cut_iT:], mc_norm[:, 0], color='blue', label='Row', linewidth=3.5)
-plt.plot(T_list[cut_iT:], mc_norm[:, 1], color='green', linewidth=3.5, label='Column')
-plt.plot(T_list[cut_iT:], sr_norm[:, 0], color='blue', linestyle='--', linewidth=3.5)
-plt.plot(T_list[cut_iT:], sr_norm[:, 1], color='green', linestyle='--', linewidth=3.5)
-plt.ylabel('NN (%)',fontsize=label_size)
-plt.xlabel('$T$', fontsize=label_size)
-plt.legend(fontsize=50, loc='upper right')
-
-ax2 = plt.subplot(122)
-plt.title('$T=2.9313$')
-N, bins, patches = plt.hist([0, 1, 2, 3], bins=4, weights=[mc_sums[20,0]+mc_sums[20,1], mc_sums[20,2], sr_sums[20,0]+sr_sums[20,1], sr_sums[20,2]])
-plt.ylim((0,150000))
-plt.xticks([0, 1, 2, 3], ['MC-NN', 'MC Diag', 'SR-NN', 'SR Diag'], fontsize=44)
-plt.yticks([])
-for i in range(0,2):
-    patches[i].set_facecolor('blue')
-for i in range(2,4):    
-    patches[i].set_facecolor('orange')
-
-ax_ins = inset_axes(ax2, 
-                    width="40%", # width = 30% of parent_bbox
-                    height="40%", # height : 1 inch
-                    loc=1)
-
-plt.plot(T_list[cut_iT:], mc_norm[:, 2], color='red', linewidth=3.5, label='MC')
-plt.plot(T_list[cut_iT:], sr_norm[:, 2], color='red', linestyle='--', linewidth=3.5, label='SR')
-plt.ylabel('Diagonal (%)', fontsize=label_size-10)
-plt.xlabel('$T$', fontsize=label_size-10)
-
-plt.savefig('block_ties_temp.pdf', bbox_inches='tight')
+#def tie_reason(states):
+#    n_samples, Ly, Lx = states.shape
+#    bs = block_sum(states)
+#    n_ties = (bs == 2).sum()
+#    keeper = (bs == 2).astype(np.int).repeat(2, axis=1).repeat(2, axis=2)
+#    
+#    tied_state = keeper * states
+#    
+#    rows = (tied_state[:, 1:] * tied_state[:, :-1])[:, np.arange(0, Ly, 2)].sum()
+#    cols = (tied_state[:, :, 1:] * tied_state[:, :, :-1])[:, :, np.arange(0, Lx, 2)].sum()
+#    
+#    return np.array([rows, cols, n_ties - rows - cols])
+#    
+#mc_sums, sr_sums = np.zeros([32, 3]), np.zeros([32, 3])
+#
+#for iT in range(32):
+#    mc = df.temp_partition(df.read_file(df.data_directory_select(1), L=16), iT)
+#    output = np.load('%s/%s/T%.4f.npy'%(output_dir, NAME, T_list[iT]))
+#    sampled = (output > np.random.random(output.shape)).astype(np.int)
+#    
+#    mc_sums[iT] = tie_reason(mc)
+#    sr_sums[iT] = tie_reason(sampled)
+#    
+#    print(iT)
+#
+#
+#rcParams.update({'font.size': 48})
+#label_size = 52
+#from mpl_toolkits.axes_grid.inset_locator import inset_axes
+#
+#
+#cut_iT = 10
+#mc_norm = 100*mc_sums[cut_iT:] / mc_sums[cut_iT:].sum(axis=1)[:, np.newaxis]
+#sr_norm = 100*sr_sums[cut_iT:] / sr_sums[cut_iT:].sum(axis=1)[:, np.newaxis]
+#
+#plt.figure(figsize=(30, 8))
+#plt.subplot(121)
+#plt.plot(T_list[cut_iT:], mc_norm[:, 0], color='blue', label='Row', linewidth=3.5)
+#plt.plot(T_list[cut_iT:], mc_norm[:, 1], color='green', linewidth=3.5, label='Column')
+#plt.plot(T_list[cut_iT:], sr_norm[:, 0], color='blue', linestyle='--', linewidth=3.5)
+#plt.plot(T_list[cut_iT:], sr_norm[:, 1], color='green', linestyle='--', linewidth=3.5)
+#plt.ylabel('NN (%)',fontsize=label_size)
+#plt.xlabel('$T$', fontsize=label_size)
+#plt.legend(fontsize=50, loc='upper right')
+#
+#ax2 = plt.subplot(122)
+#plt.title('$T=2.9313$')
+#N, bins, patches = plt.hist([0, 1, 2, 3], bins=4, weights=[mc_sums[20,0]+mc_sums[20,1], mc_sums[20,2], sr_sums[20,0]+sr_sums[20,1], sr_sums[20,2]])
+#plt.ylim((0,150000))
+#plt.xticks([0, 1, 2, 3], ['MC-NN', 'MC Diag', 'SR-NN', 'SR Diag'], fontsize=44)
+#plt.yticks([])
+#for i in range(0,2):
+#    patches[i].set_facecolor('blue')
+#for i in range(2,4):    
+#    patches[i].set_facecolor('orange')
+#
+#ax_ins = inset_axes(ax2, 
+#                    width="40%", # width = 30% of parent_bbox
+#                    height="40%", # height : 1 inch
+#                    loc=1)
+#
+#plt.plot(T_list[cut_iT:], mc_norm[:, 2], color='red', linewidth=3.5, label='MC')
+#plt.plot(T_list[cut_iT:], sr_norm[:, 2], color='red', linestyle='--', linewidth=3.5, label='SR')
+#plt.ylabel('Diagonal (%)', fontsize=label_size-10)
+#plt.xlabel('$T$', fontsize=label_size-10)
+#
+#plt.savefig('block_ties_temp.pdf', bbox_inches='tight')
