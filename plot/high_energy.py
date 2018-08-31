ad5964701f94d7bd4d770cc0c354852ba8da2d79
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-08-31 18:35:34 +0200

    playing

--- plot/high_energy.py
+++ plot/high_energy.py
@@ -18,7 +18,7 @@ def read_sr(name):
     if len(sr.shape) > 3:
         sr = sr[:, :, :, 0]
         
-    return (sr > np.random.random(sr.shape)).astype(np.int)
+    return sr
 
 def calc_nn(states):
     x = 2 * states - 1
@@ -35,18 +35,21 @@ def calc_nn(states):
 
 iT = 20
 mc = df.temp_partition(df.read_file(df.data_directory_select(3), L=16), iT)
-sr = read_sr('Simple2D16relu_L2_64_32_K333_PBC_MReg0.00EReg0.30B1000')
+sr = read_sr('FeatExt1L2D16relu_L2_32_32_K353_PBC_MReg0.00EReg0.00B1000')
 sr_old = read_sr('Simple2D16relu_L3_64_16_16_K3333_MReg0.10EReg0.30B1000_OLD')
 
-nn_list = [calc_nn(x) for x in [mc, sr, sr_old]]
+sr_s = (sr > np.random.random(sr.shape)).astype(np.int)
+sr_old_s = (sr_old > np.random.random(sr_old.shape)).astype(np.int)
+
+nn_list = [calc_nn(x) for x in [mc, sr_s, sr_old_s]]
 
 plt.figure(figsize=(10, 4))
 plt.subplot(131)
 plt.hist(block_sum(mc).ravel(), bins=5)
 plt.ylim((0,350000))
 plt.subplot(132)
-plt.hist(block_sum(sr).ravel(), bins=5)
+plt.hist(block_sum(sr_s).ravel(), bins=5)
 plt.ylim((0,350000))
 plt.subplot(133)
-plt.hist(block_sum(sr_old).ravel(), bins=5)
+plt.hist(block_sum(sr_old_s).ravel(), bins=5)
 plt.ylim((0,350000))
