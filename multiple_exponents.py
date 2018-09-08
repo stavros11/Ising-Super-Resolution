9983c34096176bcecc31258a75518640b7161898
stavros11 <stavros11@users.noreply.github.com>
2018-09-08 20:45:03 +0200

    Fixed

--- multiple_exponents.py
+++ multiple_exponents.py
@@ -6,11 +6,10 @@ Created on Sat Jun 16 18:59:45 2018
 """
 
 import numpy as np
-from data.loaders import TrainingData
-from data.directories import models_critical_save_dir
-from data.directories import multiple_exponents_dir
-from networks.utils import set_GPU_memory, load_model_from_list
-from networks.train import create_directory
+from data.loaders import read_file_GPU, read_file_critical, add_index
+from data.directories import multiple_exponents_test_dir
+from data.model_loader import ModelLoader
+from networks.utils import set_GPU_memory, create_directory
 from networks.consecutive import upsampling
 from argparse import ArgumentParser
 from scipy.stats import linregress
@@ -19,13 +18,18 @@ parser = ArgumentParser()
 parser.add_argument('-C', type=int, default=1, help='number of calculations')
 parser.add_argument('-UP', type=int, default=3, help='number of upsamplings')
 parser.add_argument('-PRreg', type=bool, default=True, help='print regression')
-parser.add_argument('-TPF', type=bool, default=False, help='calculate two point function')
-parser.add_argument('-CORR', type=bool, default=False, help='calculate correlation length')
+parser.add_argument('-TPF', type=bool, default=True, help='calculate two point function')
+parser.add_argument('-CORR', type=bool, default=True, help='calculate correlation length')
 
-parser.add_argument('-NET', type=int, default=1, help='network choice')
-parser.add_argument('-GPUF', type=float, default=0.4, help='GPU memory fraction')
+parser.add_argument('-NET', type=int, default=0, help='network choice')
+## These should be consistent with selected model ##
+parser.add_argument('-PBC', type=bool, default=True, help='use PBC padding')
+parser.add_argument('-ACT', type=str, default='relu', help='hidden activation')
+parser.add_argument('-HF', nargs='+', type=int, default=None, help='hidden filters list')
+parser.add_argument('-K', nargs='+', type=int, default=None, help='kernels list')
+
+parser.add_argument('-GPU', type=float, default=0.3, help='GPU memory fraction')
 parser.add_argument('-L', type=int, default=16, help='output size')
-parser.add_argument('-nTR', type=int, default=40000, help='train samples')
 parser.add_argument('-nTE', type=int, default=100000, help='test samples')
 parser.add_argument('-VER', type=int, default=1, help='version for name')
 
@@ -33,18 +37,20 @@ args = parser.parse_args()
 args.CR = True
 
 
-set_GPU_memory(fraction=args.GPUF)
+set_GPU_memory(fraction=args.GPU)
 if args.PRreg:
     L0 = int(np.log2(args.L))
     L_list = 2**np.arange(L0, L0+1+args.UP)
 
 ## Load model ##
-name, model = load_model_from_list(models_critical_save_dir, args.NET)
+model = ModelLoader(args.NET, critical=True)
 
-data = TrainingData(args)
+data = add_index(read_file_GPU(L=args.L))
+#data = add_index(read_file_critical(L=args.L, n_samples=args.nTE))
+print(data.shape)
 observables = []
 for iC in range(args.C):
-    obs = upsampling(data.test_out, model, args)
+    obs = upsampling(data, model.graph, args)
     observables.append(obs)
     
     if args.PRreg:
@@ -58,6 +64,6 @@ for iC in range(args.C):
             print('Eta2:')
             print(linregress(np.log10(L_list/4.0), np.log10(obs[8])))
 
-create_directory(multiple_exponents_dir)
-np.save('%s/%s_C%dUP%d.npy'%(multiple_exponents_dir, name,
+create_directory(multiple_exponents_test_dir)
+np.save('%s/%s_C%dUP%d.npy'%(multiple_exponents_test_dir, model.name,
                              args.C, args.UP), np.array(observables))
